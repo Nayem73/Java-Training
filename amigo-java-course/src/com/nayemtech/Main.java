44ea349dd1c89797c64d17835c38ec55d3dbd032
nayem73 <nayem73@gmail.com>
2023-10-24 18:00:56 +0600

    pointA is an object

--- amigo-java-course/src/com/nayemtech/Main.java
+++ amigo-java-course/src/com/nayemtech/Main.java
@@ -1,15 +1,10 @@
 package com.nayemtech;
 
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        int amount = 23_33_3;
-        System.out.println(amount);
-        String s1 = "prefix";
-        String s2 = "suffix";
-        String s = s1.toUpperCase();
-        s += s2;
-        String sTmp = "  a  b  cde f  ";
-        System.out.println(sTmp.trim());
+        Point pointA = new Point(2,5);
+        System.out.println(pointA.getX());
     }
 }
