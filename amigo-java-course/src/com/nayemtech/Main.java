ba5ad1dbfb4a3cb8453e29d6252a295c970c1f39
nayem73 <nayem73@gmail.com>
2023-10-24 04:15:26 -0700

    practicing java String (String is not a primitive data type. It's a reference data type - all those data types starting with capital letter.)

---
+++ amigo-java-course/src/com/nayemtech/Main.java
@@ -0,0 +1,15 @@
+package com.nayemtech;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int amount = 23_33_3;
+        System.out.println(amount);
+        String s1 = "prefix";
+        String s2 = "suffix";
+        String s = s1.toUpperCase();
+        s += s2;
+        String sTmp = "  a  b  cde f  ";
+        System.out.println(sTmp.trim());
+    }
+}
