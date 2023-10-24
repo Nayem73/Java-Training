bc077bc394099b74fde2a5f4ead7c354b3803892
nayem73 <nayem73@gmail.com>
2023-10-24 22:51:11 +0600

    jegulo heap e rakha hoy(reference type) oigular ekta change korle onnotao change hoy. This is also pass by value. not pass by reference.

--- amigo-java-course/src/com/nayemtech/Main.java
+++ amigo-java-course/src/com/nayemtech/Main.java
@@ -5,6 +5,11 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         Point pointA = new Point(2,5);
-        System.out.println(pointA.getX());
+        Point pointB = pointA;
+
+        pointA.x = 100;
+
+        System.out.println(pointA);
+        System.out.println(pointB);
     }
 }
