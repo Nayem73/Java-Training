bc63bf63c4c026e32775d859a12171ab6f9b06b4
nayem73 <nayem73@gmail.com>
2023-10-25 17:08:07 +0600

    for primitives default values are 0 and for Reference types, default value is null

--- amigo-java-course/src/com/nayemtech/Main.java
+++ amigo-java-course/src/com/nayemtech/Main.java
@@ -5,20 +5,12 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = new int[3];
-        numbers[0] = 21;
-        numbers[1] = 6;
-        numbers[2] = 42;
-
-        System.out.println(numbers[0]);
-        System.out.println(Arrays.toString(numbers));
-
-
-        int[] numbers2 = {3,4,6,7};
-        System.out.println(Arrays.toString(numbers2));
-
         int[] numbers3 = new int[12];
-        Arrays.fill(numbers3, -1);
         System.out.println(Arrays.toString(numbers3));
+
+        //Array of Strings
+        String[] s = new String[5];
+        s[2] = "Nayem";
+        System.out.println(Arrays.toString(s));
     }
 }
