366fd2bc9439729ff0b5832d5d600cf47acbb079
nayem73 <nayem73@gmail.com>
2023-11-02 22:23:07 +0600

    never use double in money related matter. use BigDecimal instead

--- amigo-java-course/src/com/nayemtech/Main.java
+++ amigo-java-course/src/com/nayemtech/Main.java
@@ -2,16 +2,9 @@ package com.nayemtech;
 
 public class Main {
     public static void main(String[] args) {
-        String numToString = String.valueOf(56); //convert Integer to String
-        System.out.println(numToString);
-
-        int num = 23;
-
-        String formattedString = String.format("like printing Strings in C & extra feature: numToString=%s, num=%s\n", numToString, num);
-        System.out.println(formattedString);
-
-        String[] s1 = {"cat", "tiger", "elephant"};
-        String s2 = String.join(" | ", s1);
-        System.out.println(s2);
+        double x = 0.03;
+        double y = 0.02;
+        double z = x - y;
+        System.out.println(z);
     }
 }
