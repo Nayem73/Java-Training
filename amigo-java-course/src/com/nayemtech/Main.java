fe668d5acf398dcbd98f39db901bd69862bc0f01
nayem73 <nayem73@gmail.com>
2023-11-03 00:08:13 +0600

    if above try fails, anything under this line will not be executed.

--- amigo-java-course/src/com/nayemtech/Main.java
+++ amigo-java-course/src/com/nayemtech/Main.java
@@ -5,7 +5,9 @@ import java.math.BigDecimal;
 public class Main {
     public static void main(String[] args) {
         try {
-            int number = Integer.parseInt("18");
+            int number = Integer.parseInt("18il");
+            //if above try fails, anything under this line will not be executed.
+            System.out.println(number);
         } catch (NumberFormatException e) {
             System.out.println("Failed to parse");
         }
