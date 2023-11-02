49d78704c0a265493dca45783701c48dd025021c
nayem73 <nayem73@gmail.com>
2023-11-02 22:27:46 +0600

    even BigDecimal is not providing 100% accurate but better than double. Do I need to pass String instead of number like amigos?

--- amigo-java-course/src/com/nayemtech/Main.java
+++ amigo-java-course/src/com/nayemtech/Main.java
@@ -1,10 +1,17 @@
 package com.nayemtech;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
         double x = 0.03;
         double y = 0.02;
         double z = x - y;
         System.out.println(z);
+
+        BigDecimal num1 = new BigDecimal(0.03);
+        BigDecimal num2 = new BigDecimal(0.02);
+        BigDecimal num3 = num1.subtract(num2);
+        System.out.println(num3);
     }
 }
