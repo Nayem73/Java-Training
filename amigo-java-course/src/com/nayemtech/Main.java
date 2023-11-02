5ceaa899203a24cea232de59e16225b2eae69f75
nayem73 <nayem73@gmail.com>
2023-11-02 15:38:40 +0600

    some checking with Integer Arrays (not int)

--- amigo-java-course/src/com/nayemtech/Main.java
+++ amigo-java-course/src/com/nayemtech/Main.java
@@ -1,12 +1,20 @@
 package com.nayemtech;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        String s = "whatGives";
+//        String s = "whatGives";
+        Integer[] s = new Integer[5];
+//        Arrays.fill(s, 0);
+
         String t;
-        for (int i = 0; i < (int)s.length(); i++) {
-            if (s[i] == 'v') {
-                s[i] = '5';
+        for (int i = 0; i < 5; i++) {
+            if (s[i] == null) {
+                System.out.println(i);
+            } else {
+                System.out.println(i + " " + s[i]);
             }
         }
         System.out.println(s);
