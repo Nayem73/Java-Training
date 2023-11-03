08c312b8ac670a877c13162ea2087b6b049004ff
nayem73 <nayem73@gmail.com>
2023-11-03 08:59:21 +0600

    another approach with e.getMessage(): catch (NumberFormatException | ArithmeticException e) {             System.out.println(e.getMessage());         }

--- amigo-java-course/src/com/nayemtech/Main.java
+++ amigo-java-course/src/com/nayemtech/Main.java
@@ -6,14 +6,12 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
         try {
-            int number = Integer.parseInt("18");
+            int y = 5/0;
+            int number = Integer.parseInt("18ui");
             //if above try fails, anything under this line will not be executed.
             System.out.println(number);
-            int y = 5/0;
-        } catch (NumberFormatException e) {
-            System.out.println("Failed to parse");
-        } catch (ArithmeticException e) {
-            System.out.println("Arithmetic Exception occured");
+        } catch (NumberFormatException | ArithmeticException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
