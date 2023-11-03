f52dd5e6803096846ff70c14a8c3fc4128d11a7c
nayem73 <nayem73@gmail.com>
2023-11-03 08:50:08 +0600

    catching multiple exceptions with multiple catch block (which is tedious though)

--- amigo-java-course/src/com/nayemtech/Main.java
+++ amigo-java-course/src/com/nayemtech/Main.java
@@ -1,18 +1,19 @@
 package com.nayemtech;
 
 import java.math.BigDecimal;
+import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            int number = Integer.parseInt("18il");
+            int number = Integer.parseInt("18");
             //if above try fails, anything under this line will not be executed.
             System.out.println(number);
+            int y = 5/0;
         } catch (NumberFormatException e) {
             System.out.println("Failed to parse");
+        } catch (ArithmeticException e) {
+            System.out.println("Arithmetic Exception occured");
         }
-
-        System.out.println("number can not be printed though! cz it is inside try block");
-        //System.out.println(number); can't
     }
 }
