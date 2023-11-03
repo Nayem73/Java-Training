640f6e969be7bc017ebdd4286a863365356d763b
nayem73 <nayem73@gmail.com>
2023-11-03 09:03:41 +0600

    This is better. No more being tedious: catch (Exception e) {             System.out.println(e.getMessage());         }

--- amigo-java-course/src/com/nayemtech/Main.java
+++ amigo-java-course/src/com/nayemtech/Main.java
@@ -6,11 +6,11 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
         try {
-            int y = 5/0;
             int number = Integer.parseInt("18ui");
+            int y = 5/0;
             //if above try fails, anything under this line will not be executed.
             System.out.println(number);
-        } catch (NumberFormatException | ArithmeticException e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
