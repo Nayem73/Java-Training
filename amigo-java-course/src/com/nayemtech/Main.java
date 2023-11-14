7ad3d257d9e53ec089565aa877540d7fe9f6bd30
nayem73 <nayem73@gmail.com>
2023-11-14 06:31:45 +0600

    This works

--- amigo-java-course/src/com/nayemtech/Main.java
+++ amigo-java-course/src/com/nayemtech/Main.java
@@ -6,7 +6,7 @@ public class Main {
         try {
             int y = 5/0;
             System.out.println("After Error");
-        } catch (NumberFormatException e) {
+        } catch (NumberFormatException | ArithmeticException e) {
             System.out.println("Nayem printing the error message by e.getMessage(): " + e.getMessage());
         }
     }
