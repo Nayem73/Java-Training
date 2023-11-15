f14c6471d6591931f21708fb860f113113eaf65e
nayem73 <nayem73@gmail.com>
2023-11-15 11:15:10 +0600

    toString() to print the properties(fields) of a class

--- amigo-java-course/src/com/nayemtech/Main.java
+++ amigo-java-course/src/com/nayemtech/Main.java
@@ -2,24 +2,8 @@ package com.nayemtech;
 
 public class Main {
     public static void main(String[] args) {
-        karim();
-    }
-
-    static void karim() {
-        try{
-            john();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    static void john() throws Exception {
-        divide(10, 0);
-    }
-
-    static void divide(int a, int b) throws Exception {
-        if (b == 0) {
-            throw new Exception("can not divide by 0");
-        }
+        Cat cat = new Cat("Jupyter", 2, "White");
+        System.out.println(cat.getName());
+        System.out.println(cat);
     }
 }
