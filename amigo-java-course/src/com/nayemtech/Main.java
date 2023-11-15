31043f5300da1ebb514690775395a9a878f81e1e
nayem73 <nayem73@gmail.com>
2023-11-15 06:54:44 +0600

    this is working by adding try-catch

--- amigo-java-course/src/com/nayemtech/Main.java
+++ amigo-java-course/src/com/nayemtech/Main.java
@@ -4,10 +4,14 @@ import java.io.File;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        File file = new File("src/tmp.txt");
-        if (!file.exists()) {
-            file.createNewFile();
+    public static void main(String[] args) {
+        try {
+            File file = new File("src/tmp.txt");
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
