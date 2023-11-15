264eef757bcc697274c83d65f457290f529538b5
nayem73 <nayem73@gmail.com>
2023-11-15 06:31:21 +0600

    At this point, I'm not even allowed to compile, because it will show error. because ioexcepton is checked exception (compile time). So I either need to declare throws in the method signature or use try-catch

--- amigo-java-course/src/com/nayemtech/Main.java
+++ amigo-java-course/src/com/nayemtech/Main.java
@@ -1,13 +1,12 @@
 package com.nayemtech;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("before error");
-        try {
-            int y = 5/0;
-            System.out.println("After Error");
-        } catch (Exception e) {
-            System.out.println("Nayem printing the error message by e.getMessage(): " + e.getMessage());
+        File file = new File("src/tmp.txt");
+        if (!file.exists()) {
+            file.createNewFile();
         }
     }
 }
