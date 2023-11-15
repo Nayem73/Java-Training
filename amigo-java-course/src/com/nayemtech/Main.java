71e77bde21be12a67cf8b0a6f33c2c46d7b0e579
nayem73 <nayem73@gmail.com>
2023-11-15 06:51:44 +0600

    this is working by adding exception to the method signature with throws clause

--- amigo-java-course/src/com/nayemtech/Main.java
+++ amigo-java-course/src/com/nayemtech/Main.java
@@ -1,9 +1,10 @@
 package com.nayemtech;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         File file = new File("src/tmp.txt");
         if (!file.exists()) {
             file.createNewFile();
