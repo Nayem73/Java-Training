a0f6e9d6b25b19258dd492a28c668d2fcdee1222
nayem73 <nayem73@gmail.com>
2023-11-16 14:35:56 +0600

    Arrays in java

---
+++ amigo-java-course/src/com/nayemtech/Car.java
@@ -0,0 +1,9 @@
+package com.nayemtech;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class Car implements Serializable {
+    private String regNumber;
+    private BigDecimal price;
+}
--- amigo-java-course/src/com/nayemtech/Main.java
+++ amigo-java-course/src/com/nayemtech/Main.java
@@ -1,5 +1,7 @@
 package com.nayemtech;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Cat cat = new Cat("Jupyter", "White");
@@ -11,5 +13,14 @@ public class Main {
 
         System.out.println(cat == cat2);
         System.out.println(cat.equals(cat2));
+
+        int[] a;
+        Cat[] cats;
+        int arr[] = new int[5];
+        Cat[] cats2 = new Cat[5];
+
+        int[] a2 = {5,6};
+        System.out.println(Arrays.toString(a2));
+
     }
 }
