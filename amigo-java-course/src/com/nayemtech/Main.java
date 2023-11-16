63266b829ed4799ee599c0b84cfe37fc547b356d
nayem73 <nayem73@gmail.com>
2023-11-16 17:08:19 +0600

    static keyword means - property or method belongs to the class itself rather than the instance. For example, in below code, address is static field defined in Person class. So, in main method, when create an instance of Person class named alex or jakir, address can not be accessed through that instance: `System.out.println(jakir.getAddress());` . Because static field belongs to person so instead of instance jakir, we need to say person: `System.out.println(Person.getAddress());`

--- amigo-java-course/src/com/nayemtech/Main.java
+++ amigo-java-course/src/com/nayemtech/Main.java
@@ -4,23 +4,11 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Cat cat = new Cat("Jupyter", "White");
-        System.out.println(cat.getName());
-        System.out.println(cat);
-
-        Cat cat2 = new Cat("Jupyter", "White");
-        System.out.println(cat2);
-
-        System.out.println(cat == cat2);
-        System.out.println(cat.equals(cat2));
-
-        int[] a;
-        Cat[] cats;
-        int arr[] = new int[5];
-        Cat[] cats2 = new Cat[5];
-
-        int[] a2 = {5,6};
-        System.out.println(Arrays.toString(a2));
+        Person alex = new Person("Alex", "Hunt", 33);
+        Person jakir = new Person();
+        System.out.println(Person.getAddress());
+        System.out.println(jakir.getLastName());
+        System.out.println(Person.getAddress());
 
     }
 }
---
+++ amigo-java-course/src/com/nayemtech/Person.java
@@ -0,0 +1,49 @@
+package com.nayemtech;
+
+public class Person {
+    private static String address = "Baltic Sea";
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+
+    public static String getAddress() {
+        return address;
+    }
+
+    public static void setAddress(String address) {
+        Person.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
