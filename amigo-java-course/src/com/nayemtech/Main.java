d4060cf456d876b964ae586a488607a0f3e83f06
nayem73 <nayem73@gmail.com>
2023-11-21 06:27:56 +0600

    would you look at this! if I didn't add toString() to Car class then it wouldn't show the cars when I added System.out.println(alex);

--- amigo-java-course/src/com/nayemtech/Main.java
+++ amigo-java-course/src/com/nayemtech/Main.java
@@ -1,14 +1,23 @@
 package com.nayemtech;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Person alex = new Person("Alex", "Hunt", 33);
+        Car Mercedes = new Car("Dhk-1238492", new BigDecimal("1217000"));
+        Car BMW = new Car("Chatto-1238492", new BigDecimal("2217000"));
+
+        Person alex = new Person("Alex", "Hunt", 33, new Car[]{Mercedes, BMW});
+        System.out.println(alex);
+
+        System.out.println();
         Person jakir = new Person();
         System.out.println(Person.getAddress());
         System.out.println(jakir.getLastName());
         System.out.println(Person.getAddress());
+
+        String myName = "Nayem";
 
     }
 }
--- amigo-java-course/src/com/nayemtech/Person.java
+++ amigo-java-course/src/com/nayemtech/Person.java
@@ -1,15 +1,27 @@
 package com.nayemtech;
 
+import java.util.Arrays;
+
 public class Person {
     private static String address = "Baltic Sea";
     private String firstName;
     private String lastName;
     private int age;
+    private Car[] cars;
 
-    public Person(String firstName, String lastName, int age) {
+    public Person(String firstName, String lastName, int age, Car[] cars) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+        this.cars = cars;
+    }
+
+    public Car[] getCars() {
+        return cars;
+    }
+
+    public void setCars(Car[] cars) {
+        this.cars = cars;
     }
 
     public Person() {
@@ -45,5 +57,15 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", cars=" + Arrays.toString(cars) +
+                '}';
     }
 }
