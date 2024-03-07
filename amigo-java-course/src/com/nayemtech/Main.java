5eeb51c89806e7d25ef12fbbf6e001a1164d0ae8
nayem73 <nayem73@gmail.com>
2024-03-07 17:11:29 +0600

    implemented stack in main main method

--- amigo-java-course/src/com/nayemtech/Main.java
+++ amigo-java-course/src/com/nayemtech/Main.java
@@ -3,6 +3,7 @@ package com.nayemtech;
 import javax.swing.plaf.IconUIResource;
 import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -11,19 +12,47 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-       Customer customer1 = new Customer(3, "Nayem");
-       Customer customer2 = new Customer(3, "Nayem");
+        //1 for push
+        //2 for pop
+        //3 for top
+        //4 for size
+        //0 for exit
 
-       Set<Customer> st = new HashSet<>();
-       st.add(customer1);
-       st.add(customer2);
-
-        System.out.println(customer1 == customer2);
-        System.out.println(customer1.equals(customer2));
-
-        String s1 = new String("ok");
-        String s2 = new String("ok");
-
-        System.out.println("string: " + (s1 == s2));
+        Node head = null;
+        Node prevN = null;
+        Scanner scanner = new Scanner(System.in);
+        int stackSize = 0;
+        while (true) {
+            int operation = scanner.nextInt();
+            if (operation == 1) {
+                int insertValue = scanner.nextInt();
+                if (head == null) {
+                    head = new Node(insertValue);
+                    prevN = head;
+                } else {
+                    prevN.next = new Node(insertValue);
+                    prevN.next.prev = prevN;
+                    prevN = prevN.next;
+                }
+                stackSize++;
+            } else if (operation == 2) {
+                if (stackSize == 0) {
+                    System.out.println("stack is empty!");
+                } else {
+                    prevN = prevN.prev;
+                    stackSize--;
+                }
+            } else if (operation == 3) {
+                if (stackSize == 0) {
+                    System.out.println("stack is empty!");
+                } else {
+                    System.out.println("top = " + prevN.val);
+                }
+            } else if (operation == 4) {
+                System.out.println("stack size is = " + stackSize);
+            } else if (operation == 0) {
+                break;
+            }
+        }
     }
 }
